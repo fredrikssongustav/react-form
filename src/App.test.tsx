ed57ff1afd8ba91af3f4428a85982760ca1bd180
Gustav Fredriksson <gustav.fredriksson@tink.se>
2020-05-19 11:47:24 +0200

    test: make sure we can input stuff to our field

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { App } from "./App";
+
+it("we can input things to the fields", () => {
+  const { getByTestId } = render(<App />);
+  const field = getByTestId("first-name-input");
+
+  fireEvent.input(field, { target: { value: "Fredrik" } });
+
+  expect((field as any).value).toBe("Fredrik");
+});
 
 it("renders a form", () => {
   const { getByTestId } = render(<App />);
-  const linkElement = getByTestId("form");
-  expect(linkElement).toBeInTheDocument();
+  const form = getByTestId("form");
+  expect(form).toBeInTheDocument();
 });
