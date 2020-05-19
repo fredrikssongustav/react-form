739b8ef0963f1a51f042f5bfe40d167b6401efca
Gustav Fredriksson <gustav.fredriksson@tink.se>
2020-05-19 11:51:30 +0200

    test: test that should fail in CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,22 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import { App } from "./App";
 
+it("we see a modal if we've submitted correct data", () => {
+  const { getByTestId } = render(<App />);
+
+  fireEvent.input(getByTestId("first-name-input"), {
+    target: { value: "Fredrik" },
+  });
+  fireEvent.input(getByTestId("last-name-input"), {
+    target: { value: "Gustavsson" },
+  });
+  fireEvent.input(getByTestId("age-input"), {
+    target: { value: "42" },
+  });
+
+  expect(getByTestId("modal")).toBeInTheDocument();
+});
+
 it("we can input things to the fields", () => {
   const { getByTestId } = render(<App />);
   const field = getByTestId("first-name-input");
